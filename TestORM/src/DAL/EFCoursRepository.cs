844a410596469941483cd3df0c479f805809fed4
Samuel Harvey <shdrawings@outlook.com>
2021-02-01 21:24:16 -0500

    Numero 5
    Je n'ai pas eu le temps de faire les tests malheureusement

--- TestORM/src/DAL/EFCoursRepository.cs
+++ TestORM/src/DAL/EFCoursRepository.cs
@@ -28,5 +28,42 @@ namespace TestORMCodeFirst.DAL
         {
             return contexte.Cours.toList;   
         }
+
+        void MettreAJourNoteFinale(short etudiantID, string cours, string session, short note)
+        {
+            InscriptionCours insc = contexte.InscCours.Find(etudiantID, cours, session);
+            insc.NoteFinale = note;
+            contexte.SaveChanges();
+        }
+
+        double ObtenirPourUneClasseLaMoyenne(string cours, string session)
+        {
+            List<InscriptionCours> inscCoursListe = contexte.InscCours.Where(insc => insc.CodeSession == session && insc.CodeCours == cours);
+            double somme = 0;
+
+            foreach(inscCours.NoteFinale notes in inscCoursListe){
+                somme += notes;
+            }
+            double moyenne = somme / inscCoursListe.Count();
+
+            return moyenne;
+        }
+
+        int ObtenirPourUneClasseNombreEchecs(string cours, string session)
+        {
+            List<InscriptionCours> inscCoursListe = contexte.InscCours.Where(insc => insc.CodeSession == session && insc.CodeCours == cours);
+
+            int echecs = 0;
+
+            foreach (inscCours.NoteFinale notes in inscCoursListe)
+            {
+                if(notes < 60)
+                {
+                    echecs++;
+                }
+            }
+
+            return echecs;
+        }
     }
 }
